60fdd85229abfd023562d1d14c980b14cf133cc7
emarche <46004839+emarche@users.noreply.github.com>
2023-02-18 10:07:27 -0500

    theme light

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -71,9 +71,9 @@ let transTheme = () => {
 let initTheme = (theme) => {
   if (theme == null || theme == 'null') {
     const userPref = window.matchMedia;
-    if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
-        theme = 'dark';
-    }
+    //if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
+    //    theme = 'dark';
+    //}
   }
 
   setTheme(theme);
